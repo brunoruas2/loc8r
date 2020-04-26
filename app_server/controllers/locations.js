223e804a7d28601e71003a0ca76312d238217b9f
Bruno Ruas <bruno.ruas2@gmail.com>
2020-04-26 00:37:56 -0400

    Building a static site

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-// GET 'homelist' page
+// GET 'homelist' page - ok
 const homelist = (req, res) => {
     res.render('locations-list', { title: 'Locations' });
 };
 
-// GET 'Location info' page
+// GET 'Location info' page - ok
 const locationInfo = (req, res) => {
-    res.render('index', { title: 'Location Info' });
+    res.render('location-info', { title: 'Location Info' });
 };
 
-// GET 'Add review' page
+// GET 'Add review' page - ok
 const addReview = (req, res) => {
-    res.render('index', { title: 'Add review' });
+    res.render('location-review-form', { title: 'Add review' });
 };
 
 module.exports = {

46ee26fe3a07643c33c04b563602fcc3e670b7cf
Bruno de Melo Ruas <bruno.ruas@semsa.am.br>
2020-04-27 13:42:02 -0400

    Building a static site

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -35,36 +35,55 @@ const homelist = (req, res) => {
 // GET 'Location info' page - ok
 const locationInfo = (req, res) => {
     res.render('location-info', { 
-        title: 'Location Info' ,
+        title: 'Starcups' ,
         pageHeader: {
             title:'Starcups',
-            strapline: 'Find places to work with wify near you!'
         },
-        rating:4,
-        address:'125 High Street, Reading, RG6 1PS',
-        sidebar1: 'Starcups is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
-        sidebar2:"if you've been and you like it - or if you don't. But leave a review to help other people just like you.",
-        location_info: [{
-            opening: ['Monday: 7am-7pm','Saturday: 8am - 5pm'],
+        sidebar:{
+            context:'Starcups is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
+            callToAction:"If you've been and you like it - or if you don't. But leave a review to help other people just like you."
+        },
+        location:{
+            name:'Starcups',
+            address:'125 High Street, Reading, RG6 1PS',
+            rating:4,
             facilities:['Hot drinks','Food','Premium Wifi'],
-        }],
-        reviews: [{
-            name:'Simon Holmes',
-            date:'16 February 2017',
-            rating:3,
-            comment:'What a great place!'
-        },{
-            name:'Charlie Chaplin',
-            date:'15 February 2017',
-            rating:2,
-            comment:'Coffe gut!'
-        }]
+            coords:'iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15936.768750392297!2d-60.02027928829196!3d-3.043106171570461!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x926c1a1055f67251%3A0x9ae77ab416b67309!2sHappy%20Day%20Futebol%20Society!5e0!3m2!1spt-PT!2sbr!4v1587870062455!5m2!1spt-PT!2sbr" width="100%" height="238" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"',
+            openingTimes: [{
+                days: 'Monday- Friday',
+                opening: '7:00am',
+                closing:'7:00pm',
+                closed: false
+            },{
+                days: 'Saturday',
+                opening: '8:00am',
+                closing:'5:00pm',
+                closed: false
+            },{
+                days: 'Sunday',
+                closed: true
+            }],
+            reviews: [{
+                author: 'Simon Holmes',
+                rating: 5,
+                timestamp: '16 July 2013',
+                reviewText: 'What a great place. I can\'t say enough good things about it.'
+            },{
+                author: 'Charlie Chaplin',
+                rating: 3,
+                timestamp: '16 June 2013',
+                reviewText: 'It\'s Okay!'
+            }]
+        }
     });
 };
 
 // GET 'Add review' page - ok
 const addReview = (req, res) => {
-    res.render('location-review-form', { title: 'Add review' });
+    res.render('location-review-form', {
+        title: 'Review Stacups on Loc8r',
+        pageHeader: 'Review Starcups'
+    });       
 };
 
 module.exports = {

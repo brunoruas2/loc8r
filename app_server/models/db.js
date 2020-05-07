89ab06ef9b224eb515d5338186c18fab1b991a67
Bruno de Melo Ruas <bruno.ruas@semsa.am.br>
2020-05-07 13:50:44 -0400

    mongodb

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -3,16 +3,16 @@ const readLine = require('readline');
 require('../controllers/locations');
 
 // Creating a conecction
-
+/**
 let dbURI = 'mongodb://localhost/Loc8r';
 if (process.env.NODE_ENV === 'production') {
   dbURI = process.env.MLAB_URI;
 }
+*/
 
-/**
 const dbURI = "mongodb+srv://app:datascience135@loc8r-w0grw.gcp.mongodb.net/test";
 mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true});
-*/
+
 
 // Monitors for a successful connection through Mongoose
 mongoose.connection.on('connected', () => {

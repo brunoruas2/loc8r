3786bdc06a2709e0224c2048e2794f4439a137bc
Bruno de Melo Ruas <bruno.ruas@semsa.am.br>
2020-05-08 13:39:42 -0400

    locations db

---
+++ app_server/models/locations.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+
+// DATABASE
+// subdocument
+const openingTimeSchema = new mongoose.Schema({
+    days:{
+        type:String,
+        required:true
+    },
+    opening:String,
+    closing:String,
+    closed:{
+        type:Boolean,
+        required:true
+    }
+});
+// subdocument
+const reviewSchema = new mongoose.Schema({
+    author: String,
+    rating: {
+        type:Number,
+        required:true,
+        min:0,
+        max:5
+    },
+    reviewText:String,
+    createdOn:{
+        type:Date,
+        'default':Date.now
+    }
+});
+// document
+const locationSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required: true},
+    address:String,
+    rating:{
+        type:Number,
+        'default': 0,
+        min: 0,
+        max: 5},
+    facilities:[String],
+    coods:{
+        type:{ type: String },
+        coordinates: [Number]
+    },
+    openingTimes: [openingTimeSchema],
+    reviews: [reviewSchema]
+});
+
+locationSchema.index({coords: '2dsphere'});
+
+// Compiling a Model from a Schema
+mongoose.model('Location', locationSchema);

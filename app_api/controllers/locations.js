9bd696bede2a9ab92998c4864372fd0d602efa26
Bruno de Melo Ruas <bruno.ruas@semsa.am.br>
2020-05-14 17:48:19 -0400

    Finding Multiple documents with geospatia queries

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -1,15 +1,40 @@
 const mongoose = require('mongoose');
 const Loc = mongoose.model('Location');
 
-const locationsListByDistance = (req, res) => { };
-const locationsCreate = (req, res) => {
-  res
-    .status(200)
-    .json({"status" : "sucess"})
+const locationsListByDistance = (req, res) => {
+  Loc.aggregate([{$geoNear: {near: {}, distanceField: "distance", maxDistance: 100}}]);
 };
-const locationsReadOne = (req, res) => { };
+
+const locationsCreate = (req, res) => { };
+
+const locationsReadOne = (req, res) => {
+  // Searching the document with the locationid inputed in the url
+  Loc
+    .findById(req.params.locationid)
+    .exec((err, location) => {
+      // Error 1: location not found
+      if (!location) {
+        return res
+          .status(404)
+          .json({
+            "message" : "location not found"
+          });
+      } else if (err) { // Error 2: Mongoose return a error
+        return res
+          .status(400)
+          .json(err);
+      } else {
+        res
+        .status(200)
+        .json(location)
+      }
+    })
+};
+
 const locationsUpdateOne = (req, res) => { };
+
 const locationsDeleteOne = (req, res) => { };
+
 
 module.exports = {
   locationsListByDistance,
--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -2,9 +2,60 @@ const mongoose = require('mongoose');
 const Loc = mongoose.model('Location');
 
 const reviewsCreate = (req, res) => { };
-const reviewsReadOne = (req, res) => { };
+
+const reviewsReadOne = (req, res) => {
+    // Searching the document with the locationid inputed in the url
+    Loc
+    .findById(req.params.locationid)
+    .select('name reviews') // Adds mongoose select method for model query
+    .exec((err, location) => {
+      // Error 1: location not found
+      if (!location) {
+        return res
+          .status(404)
+          .json({
+            "message" : "location not found"
+          });
+      } else if (err) { // Error 2: Mongoose return a error
+        return res
+          .status(400)
+          .json(err);
+      }
+      // Check if location has reviews
+      if (location.reviews && location.reviews.length > 0) {
+        const review = location.reviews.id(req.params.reviewid);
+        if (!review) {
+          return res
+            .status(400)
+            .json({
+              "message" : "review not found"
+            });
+        } else {
+          response = {
+            location : {
+              name : location.name,
+              id : req.params.locationid
+            },
+            review
+          }
+          return res
+            .status(200)
+            .json(response);
+        }
+      } else {
+        return res
+          .status(404)
+          .json({
+            "message" : "No reviews found"
+          });
+        }
+    })
+};
+
 const reviewsUpdateOne = (req, res) => { };
+
 const reviewsDeleteOne = (req, res) => { };
+
 
 module.exports = {
   reviewsCreate,
